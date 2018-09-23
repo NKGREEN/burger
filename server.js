7c0089e4872621aad4f28feea7dedc7e380b1e68
NKGREEN <green.nadiak@gmail.com>
2018-09-22 23:35:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
 
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// parse 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and provide server access.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
